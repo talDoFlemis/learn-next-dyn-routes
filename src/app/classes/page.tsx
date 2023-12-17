fae6b7257f7d96a9d26a3e7e096081a3a5a1a4d9
taldoflemis <coderflemis@gmail.com>
2023-12-17 09:32:29 -0300

    feat: adicionar paginal inicial

---
+++ src/app/classes/page.tsx
@@ -0,0 +1,28 @@
+import DumbClassCard from "@/components/DumbClassCard";
+import { NextPage } from "next";
+import React from "react";
+import { getClasses, getSlowEndpoint } from "@/services/classService";
+
+const Page: NextPage = async () => {
+  // Slow Requests
+  await getSlowEndpoint(1000);
+  await getSlowEndpoint(4000);
+  await getSlowEndpoint(2000);
+
+  const classes = await getClasses();
+  return (
+    <main className="min-h-screen flex flex-col p-6 gap-6">
+      <DumbClassCard code="CSC-301" credits={3} name="Software Engineering" />
+      {classes.map((c) => (
+        <DumbClassCard
+          key={c.code}
+          code={c.code}
+          credits={c.credits}
+          name={c.name}
+        />
+      ))}
+    </main>
+  );
+};
+
+export default Page;
