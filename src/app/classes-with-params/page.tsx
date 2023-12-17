9d2884c79f2ad11cedd670e9e5e46fd7c680263e
taldoflemis <coderflemis@gmail.com>
2023-12-17 10:03:11 -0300

    feat: adicionar class-with-params rooto page

---
+++ src/app/classes-with-params/page.tsx
@@ -0,0 +1,9 @@
+const Page = async () => {
+  return (
+    <main className="min-h-screen flex flex-col p-6 gap-6">
+      <h1 className="text-4xl font-bold">Rooto Page</h1>
+    </main>
+  );
+};
+
+export default Page;
