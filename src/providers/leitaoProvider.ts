ba09d968cc92f8138f9925f3d56f22bfcb138241
taldoflemis <coderflemis@gmail.com>
2023-12-17 08:06:35 -0300

    feat: adicionar leitaoProvider

---
+++ src/providers/leitaoProvider.ts
@@ -0,0 +1,65 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+
+const BASE_API_URL = "@/utils/constants";
+
+export interface ApiResponse<T> {
+  data: T;
+  status: number;
+}
+
+interface ApiProvider {
+  get<T>(path: string, options?: AxiosRequestConfig): Promise<T>;
+  post<T, D>(path: string, data?: D, options?: AxiosRequestConfig): Promise<T>;
+  put<T, D>(path: string, data?: D, options?: AxiosRequestConfig): Promise<T>;
+  remove<T>(path: string, options?: AxiosRequestConfig): Promise<T>;
+}
+
+type CustomAxiosInstance = AxiosInstance & {
+  [key: string]: any;
+};
+
+export const createApiProvider = (): ApiProvider => {
+  const instance: CustomAxiosInstance = axios.create({
+    baseURL: BASE_API_URL,
+  });
+
+  instance.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error),
+  );
+
+  const makeRequest = async <T>(
+    method: keyof CustomAxiosInstance,
+    path: string,
+    data?: any,
+    options?: AxiosRequestConfig,
+  ): Promise<T> => {
+    const response: AxiosResponse<T> = await instance[method](
+      path,
+      data,
+      options,
+    );
+
+    if (response.status < 200 || response.status > 300) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.data;
+  };
+
+  return {
+    get: <T>(path: string, options?: AxiosRequestConfig) =>
+      makeRequest<T>("get", path, null, options),
+    post: <T, D>(path: string, data?: D, options?: AxiosRequestConfig) =>
+      makeRequest<T>("post", path, data, options),
+    put: <T, D>(path: string, data?: D, options?: AxiosRequestConfig) =>
+      makeRequest<T>("put", path, data, options),
+    remove: <T>(path: string, options?: AxiosRequestConfig) =>
+      makeRequest<T>("delete", path, null, options),
+  };
+};
