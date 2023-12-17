55fdfdeccc864368fe5407446cb563b6106f2cec
taldoflemis <coderflemis@gmail.com>
2023-12-17 17:15:25 -0300

    feat: adicionar not-found padrao

---
+++ src/app/classes/[id]/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Merda aconteceu</h2>
+      <Link href="/classes">Return Home</Link>
+    </div>
+  )
+}
